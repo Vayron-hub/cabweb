d2803e672a42b2bbf6650cb552dddc3a2c139a7a
Vayron-hub <vayron15conchas@gmail.com>
2025-07-23 08:12:48 -0600

    Debugging

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Landing } from './components/landing/landing';
+import { Login } from './components/login/login';
+import { Dashboard } from './components/dashboard/dashboard';
+import { AdminDashboard } from './components/admin-dashboard/admin-dashboard';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Landing },
+  { path: 'login', component: Login },
+  { 
+    path: 'dashboard', 
+    component: Dashboard,
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'admin', 
+    component: AdminDashboard,
+    canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '' }
+];
