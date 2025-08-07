90434bb4d2e716c7614527189499679481ae0efa
Pumpinator <alejandrodcardona5@gmail.com>
2025-08-07 13:02:40 -0600

    usar siempre el puerto 8080 en el desarollo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7286/api', // Tu backend en HTTPS
+  apiUrl: 'http://localhost:8080/api', // Tu backend en HTTPS
   apiTimeout: 30000, // 30 segundos
   enableDevTools: true,
   version: '1.0.0'
